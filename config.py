70debe62831d7542046328382deb4cd6a97e6361
kuznetsov_danil <danya.kuznetsov76@gmail.com>
2020-05-18 16:00:38 +0500

    correct config

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:docker@localhost:32768/event_planning'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:docker@localhost:32769/event_planning'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)
